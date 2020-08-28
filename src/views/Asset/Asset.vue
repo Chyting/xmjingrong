761e7e6d9a196fd46f1cec487857773d0aeac0f4
huyt <15291518099@163.com>
2020-08-28 14:04:17 +0800

    新建了页面及路由

---
+++ src/views/Asset/Asset.vue
@@ -0,0 +1,11 @@
+<template>
+  <div id="asset"></div>
+</template>
+<script>
+export default {
+  name: 'Asset'
+};
+</script>
+<style lang="scss">
+@import './index.scss';
+</style>
---
+++ src/views/Insurance/Insurance.vue
@@ -0,0 +1,173 @@
+<template>
+  <div id="insurance">
+    <div class="banner">
+      <img src="https://ts.market.mi-img.com/thumbnail/jpeg/q80/Finance/0ec825e5978c1788261f66c98abc608dd0843b145" alt="" />
+    </div>
+    <div class="main">
+      <div class="inner clear">
+        <div class="it">
+          <div class="product product1">
+            <div class="title-wrap">
+              <div class="m">健康险</div>
+              <div class="n">精选产品，全面守护</div>
+            </div>
+            <div class="cont-item">
+              <div class="title">成人优选</div>
+              <div class="cont clear">
+                <div class="im">
+                  <img src="https://ts.market.mi-img.com/thumbnail/webp/q100/Finance/0049e58fd81a324bf8410ae04e09b725f4241f4b1" alt="" />
+                  <div class="name">· 米医保·百万医疗险 ·</div>
+                  <div class="desc">600万保障 报销住院医药费 65岁高龄可投</div>
+                  <div class="price">首月1元(限时)</div>
+                </div>
+                <div class="im">
+                  <img src="https://ts.market.mi-img.com/thumbnail/webp/q100/Finance/03a75490ff16a6744d19c38f9b4faea2a3341aa7a" alt="" />
+                  <div class="name">· 小米防癌卫士医疗险 ·</div>
+                  <div class="desc">200万保额 不限三高 28天-70岁可投</div>
+                  <div class="price">5元/月起</div>
+                </div>
+                <div class="im">
+                  <img src="https://ts.market.mi-img.com/thumbnail/webp/q100/Finance/00a0e5ccd8390638d9797b3c1041fd71352401027" alt="" />
+                  <div class="name">· 小米国民重疾险(康惠保) ·</div>
+                  <div class="desc">保障130种疾病 重疾险界的小米手机</div>
+                  <div class="price">180元/年起</div>
+                </div>
+              </div>
+            </div>
+            <div class="cont-item">
+              <div class="title">孝敬父母</div>
+              <div class="cont clear">
+                <div class="im">
+                  <img src="https://ts.market.mi-img.com/thumbnail/webp/q100/Finance/02c6305576cc347953665e42e8a4a106ba0951c28" alt="" />
+                  <div class="name">· 安享一生癌症医疗险 ·</div>
+                  <div class="desc">不限三高 28天-70岁可投 200万保额</div>
+                  <div class="price">4元/月起</div>
+                </div>
+                <div class="im">
+                  <img src="https://ts.market.mi-img.com/thumbnail/webp/q100/Finance/0ddc15104187c845bc5ea3f97ba7b340b0c42f309" alt="" />
+                  <div class="name">· 中老年长期防癌保 ·</div>
+                  <div class="desc">免体检99%癌症都保45-75岁均保</div>
+                  <div class="price">19.25元/月起</div>
+                </div>
+                <div class="im">
+                  <img src="https://ts.market.mi-img.com/thumbnail/webp/q100/Finance/0749e15fd218344b214106e04419bb22f8af605f0" alt="" />
+                  <div class="name">· 老人防癌医疗险 ·</div>
+                  <div class="desc">1.3元/天起 糖尿病/三高/心血管人群也可保</div>
+                  <div class="price">449元/年起</div>
+                </div>
+              </div>
+            </div>
+            <div class="cont-item">
+              <div class="title">守护孩子</div>
+              <div class="cont clear">
+                <div class="im">
+                  <img src="https://ts.market.mi-img.com/thumbnail/webp/q100/Finance/08c635a57cc0347997665342eb14a90cb76432810" alt="" />
+                  <div class="name">· 小米少儿住院保 ·</div>
+                  <div class="desc">社保范围内用药至少报销80% 不限赔付次数</div>
+                  <div class="price">200元/月起</div>
+                </div>
+                <div class="im">
+                  <img src="https://ts.market.mi-img.com/thumbnail/webp/q100/Finance/01c6305576cc347943665242e894a100ba09a1c28" alt="" />
+                  <div class="name">· 哆啦A保终身重疾险 ·</div>
+                  <div class="desc">多次赔付 160种疾病 60万保额</div>
+                  <div class="price">570元/年起</div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="product product2">
+            <div class="title-wrap">
+              <div class="m">意外险</div>
+              <div class="n">小小投入，十分保障</div>
+            </div>
+            <div class="cont-item">
+              <div class="title">成人优选</div>
+              <div class="cont clear">
+                <div class="im">
+                  <img src="https://ts.market.mi-img.com/thumbnail/webp/q100/Finance/0849e15fd218344b214105e04479bb26f8afe05f0" alt="" />
+                  <div class="name">· 小米综合意外险 ·</div>
+                  <div class="desc">大地财险承保 保障意外身故、伤残、医疗和猝死</div>
+                  <div class="price">9.9元/年起</div>
+                </div>
+                <div class="im">
+                  <img src="https://ts.market.mi-img.com/thumbnail/webp/q100/Finance/0049e58fd81a324bfc4106e04109b725f3241f4b1" alt="" />
+                  <div class="name">· 小米架乘保 ·</div>
+                  <div class="desc">上车即可保！全车5座都保！保额合计59.5万！</div>
+                  <div class="price">10元起</div>
+                </div>
+                <div class="im">
+                  <img src="https://ts.market.mi-img.com/thumbnail/webp/q100/Finance/0fdc19504c80c445725ea1f973e7b04eb17baa0fd" alt="" />
+                  <div class="name">· 高风险运动意外保 ·</div>
+                  <div class="desc">高性价比 保高风险运动 保猝死</div>
+                  <div class="price">5.00元起</div>
+                </div>
+              </div>
+            </div>
+            <div class="cont-item">
+              <div class="title">孝敬父母</div>
+              <div class="cont clear">
+                <div class="im">
+                  <img src="https://ts.market.mi-img.com/thumbnail/webp/q100/Finance/03c6305576cc347943665642e864a105ba0941c28" alt="" />
+                  <div class="name">· 小米老人意外险 ·</div>
+                  <div class="desc">50-90周岁可保 | 老人意外风险覆盖全面</div>
+                  <div class="price">首月1元（限时）</div>
+                </div>
+              </div>
+            </div>
+            <div class="cont-item">
+              <div class="title">守护孩子</div>
+              <div class="cont clear">
+                <div class="im">
+                  <img src="https://ts.market.mi-img.com/thumbnail/webp/q100/Finance/08c635a57cc0347995665a42e014af0cb76432810" alt="" />
+                  <div class="name">· 米宝保综合意外险 ·</div>
+                  <div class="desc">保意外医疗、疫苗风险、淘气造成他人损失</div>
+                  <div class="price">99元/月起</div>
+                </div>
+                <div class="im">
+                  <img src="https://ts.market.mi-img.com/thumbnail/webp/q100/Finance/08c635a57cc035799f665242e514ab0cb36432810" alt="" />
+                  <div class="name">· 熊宝保综合意外险 ·</div>
+                  <div class="desc">意外与公共场所责任全保障</div>
+                  <div class="price">0.19元/天</div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="product product3">
+            <div class="title-wrap">
+              <div class="m">财产险</div>
+              <div class="n">贴心保障，售后无忧</div>
+            </div>
+            <div class="cont-item">
+              <div class="cont clear">
+                <div class="im">
+                  <img src="https://ts.market.mi-img.com/thumbnail/webp/q100/Finance/08c635a57cc0347990665042ec14a70cb86432810" alt="" />
+                  <div class="name">· 平安手机延保 ·</div>
+                  <div class="desc">质保延一年 免费维修 支持换新</div>
+                  <div class="price">9.90元起</div>
+                </div>
+                <div class="im">
+                  <img src="https://ts.market.mi-img.com/thumbnail/webp/q100/Finance/0ddc15104087c645b95eaaf975a7b440b4c42f309" alt="" />
+                  <div class="name">· 平安碎屏险 ·</div>
+                  <div class="desc">15天新机均能买</div>
+                  <div class="price">66.00元起</div>
+                </div>
+                <div class="im">
+                  <img src="https://ts.market.mi-img.com/thumbnail/webp/q100/Finance/0ec6305576cc347933665542e8a4a108ba0961c28" alt="" />
+                  <div class="name">· 小米随车保 ·</div>
+                  <div class="desc">车中重要物品 丢失有保障</div>
+                  <div class="price">19.90元</div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+export default {};
+</script>
+<style lang="scss">
+@import './index.scss';
+</style>
