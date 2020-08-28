b7611e31e537f2d801fbdfef1569d66e00585a8b
huyt <15291518099@163.com>
2020-08-28 10:19:19 +0800

    完成jlist/tabr组件

---
+++ src/views/Wealth/Jlist/Jlist.vue
@@ -0,0 +1,21 @@
+<template>
+  <li>
+    <a href="#">
+      <h3>{{jjlist.h3}}</h3>
+      <div class="price"><span>{{jjlist.price_d}}</span>.{{jjlist.price_x}}</div>
+      <p>{{jjlist.lilv}}</p>
+      <div class="btm">
+        <p class="txt">{{jjlist.text_d}}<i class="deliver"></i>{{jjlist.text_x}}</p>
+      </div>
+    </a>
+  </li>
+</template>
+<script>
+export default {
+  name: 'Jlist',
+  props: ['jjlist']
+};
+</script>
+<style lang="scss" >
+@import './index.scss';
+</style>
